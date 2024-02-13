a253323698c5c12e70a7fc4c5a5bc63dc2563f74
kkk1k <googija9818@gmail.com>
2024-02-13 22:54:26 +0900

    ✨ Image section 컴포넌트 추가
    Image section 컴포넌트 추가

--- src/components/Camp/CampImageSection.jsx
+++ src/components/Camp/CampImageSection.jsx
@@ -1,24 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+import theme from '../../theme';
 
-function CampImageSection() {
+const Section = styled.section`
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url(${props => props.src});
+  background-position: center;
+  background-size: ${props => (props.size ? props.size : 'cover')};
+  background-repeat: no-repeat;
+`;
+
+const H2 = styled.h2`
+  font-size: ${props => (props.size ? props.size : '1.5625rem')};
+  color: ${props => (props.color ? theme.colors[props.color] : theme.colors.white)};
+  text-align: ${props => (props.align ? props.align : 'center')};
+  padding: ${props => (props.padding ? props.padding : '')};
+  font-weight: bold;
+`;
+
+const P = styled.p`
+  font-size: ${props => (props.size ? props.size : '1.25rem')};
+  color: ${props => (props.color ? theme.colors[props.color] : theme.colors.white)};
+  word-break: keep-all;
+  grid-area: desc;
+  line-height: 1.3;
+  text-align: ${props => (props.align ? props.align : '')};
+  padding: ${props => (props.padding ? props.padding : '')};
+`;
+
+function CampImageSection({img, title, label1 = '', label2 = '', label3 = ''}) {
   return (
-    <div>
-      <BackgroundImage position='relative' height='80vh' src={campBackgroundImage1}>
-        <FlexCenter pl='10%' pr='10%'>
-          <H3 margin='0 0 2rem 0'>ON/OFF blended</H3>
-          <Span margin='0 0 1rem 0' size='1.25rem'>
-            본 캠프 2박 3일을 포함하여
-          </Span>
-          <Span margin='0 0 1rem 0' size='1.25rem'>
-            사전 데모데이를 통해{' '}
-          </Span>
-          <Span margin='0 0 1rem 0' size='1.25rem'>
-            {' '}
-            팀원들과 친해지세요.
-          </Span>
-        </FlexCenter>
-      </BackgroundImage>
-    </div>
+    <Section src={img}>
+      <H2 padding='0 10% 2rem 10%'>{title}</H2>
+      <P padding='0 10% 1rem 10%'>{label1}</P>
+      <P padding='0 10% 1rem 10%'>{label2}</P>
+      <P padding='0 10% 1rem 10%'>{label3}</P>
+    </Section>
   );
 }
 
--- src/pages/CampPage.jsx
+++ src/pages/CampPage.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import CampMainSection from '../components/Camp/CampMainSection';
 import CampIntroSection from '../components/Camp/CampIntroSection';
+import Camp1 from '../assets/camp1.png';
+import Camp2 from '../assets/camp2.png';
+import Camp3 from '../assets/camp3.png';
+import CampImageSection from '../components/Camp/CampImageSection';
 
 function CampPage() {
   return (
     <main>
       <CampMainSection />
       <CampIntroSection />
+      <CampImageSection
+        title='ON/OFF blended'
+        img={Camp1}
+        label1='본 캠프는 2박 3일을 포함하여'
+        label2='사전 데모데이를 통해 팀원들과'
+        label3='친해지세요.'
+      />
+      <CampImageSection
+        title='Various Program'
+        img={Camp2}
+        label1='다양한 인적 교류 활성화'
+        label2='프로그램을 진행합니다.'
+      />
+      <CampImageSection
+        title='Connection with Professor'
+        img={Camp3}
+        label1='산업공학과 출신 기업인,'
+        label2='교수님과 소통하세요'
+      />
     </main>
   );
 }
