065d000bb3145a74fa5892b331603aeadee6bf6e
bobob0311 <qweasdzxc0311@naver.com>
2024-02-14 23:18:21 +0900

    :lipstick: Contact form 디자인
    form 드롭다운, 경고에 사용하는 별 위치 제외 완성

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,15 +1,23 @@
 import styled from 'styled-components';
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
 import theme from '../../theme';
 
-const Form = styled.form`
-  padding: 3% 1%;
+const ContactSection = styled.section`
   background: ${theme.colors.white};
   border-radius: 1rem;
 `;
 
+const Form = styled.form`
+  padding: 3% 1%;
+  background-image: linear-gradient(to bottom, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.4)),
+    url('fieldLogo.png');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 50% auto;
+`;
+
 const InputLabel = styled.label`
-  font-size: 1.25rem;
+  font-size: 1.2rem;
   margin: 1rem 0 0 0;
   display: block;
   color: ${theme.colors.black};
@@ -18,47 +26,52 @@ const InputLabel = styled.label`
 `;
 
 const TypeLabel = styled(InputLabel)`
-  margin: 1.2rem 0 2rem 0;
+  position: absolute;
+  margin: 0.8rem 0 0 0;
   display: flex;
-  align-items: center;
+  right: 11%;
 `;
 
 const TypeSelect = styled.select`
   color: ${theme.colors.black};
   appearance: none;
-  background: url('down.png') no-repeat 100% 50%;
-  background-size: 1.4rem 1rem;
-  border: none;
+  border: 0.13rem solid ${theme.colors.black};
   font-size: 1rem;
-  font-weight: 500;
-  border-bottom: 0.1rem solid black;
-  margin: 0 0 0 0.5rem;
-  height: 1.5rem;
-  padding: 0.3rem 0 0 0.2rem;
-  width: 3.8rem;
+  font-weight: 600;
+  background: url('Expand_down.png') no-repeat 100% 10%;
+  background-size: 2rem 2rem;
+  border-radius: 0.7rem;
+  padding: 0 0 0 0.5rem;
+  width: 7.3rem;
+  height: 2.4rem;
 `;
 
 const Input = styled.input`
   color: ${theme.colors.black};
   font-size: 1rem;
+  font-weight: 600;
   width: 100%;
-  height: 2rem;
+  height: 1.7rem;
   border: none;
   border-bottom: 0.15rem solid ${theme.colors.black};
-  padding: 0.5rem 0.2rem 0 0.2rem;
+  padding: 0rem 0.2rem 0 0.1rem;
   box-sizing: border-box;
+  background: none;
 `;
 
 const TextArea = styled.textarea`
+  padding: 0.4rem;
   width: 100%;
   border: none;
   margin: 0.8rem 0 0 0;
   box-sizing: border-box;
   border-radius: 0.7rem;
   border: 0.15rem solid ${theme.colors.black};
+  background: none;
+  font-weight: 600;
 `;
 
-const SummitButton = styled.button`
+const SubmitButton = styled.button`
   background: ${theme.colors.white};
   font-size: 1.25rem;
   color: ${theme.colors.black};
@@ -67,64 +80,175 @@ const SummitButton = styled.button`
   border: 0.05rem solid ${theme.colors.black};
   border-radius: 1rem;
   margin: 1.5rem auto;
-  padding: 0.8rem 1.5rem;
+  padding: 0.65rem 0.8rem 0.5rem 0.8rem;
   display: block;
   font-weight: 600;
   box-shadow: 0.5rem 0.5rem 0.5rem rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+`;
+const ButtonImg = styled.img`
+  widht: 1.5rem;
+  height: 1.5rem;
+  padding: 0 0 0.1rem 0;
+  margin: 0 0 0 0.1rem;
+`;
+const Img = styled.img`
+  height: 1.8rem;
+  margin: 0 0 0 0.2rem;
+`;
+const VerticalCenter = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
-const Option = styled.option``;
+const Option = styled.option`
+  appearance: none;
+`;
+
+const Notice = styled.span`
+  position: relative;
+
+  &::before {
+    content: '*';
+    color: #ff0202;
+    position: absolute;
+    margin: 0;
+    font-size: 1.5rem;
+    bottom: -0.5rem;
+  }
+`;
+
+const initialValidationState = {
+  isNameValid: undefined,
+  isPhoneValid: undefined,
+  isEmailValid: undefined,
+  isTitleValid: undefined,
+  isContentValid: undefined,
+};
 
 export default function ContactForm() {
-  const {
-    current: {type, name, email, phoneNumber, content, title},
-  } = useRef({});
+  const enteredData = {
+    type: useRef(null),
+    name: useRef(null),
+    email: useRef(null),
+    phoneNumber: useRef(null),
+    content: useRef(null),
+    title: useRef(null),
+  };
 
-  const submittedData = {
-    type: type ? type.current.value : '',
-    name: name ? name.current.value : '',
-    email: email ? email.current.value : '',
-    phoneNumber: phoneNumber ? phoneNumber.current.value : '',
-    title: title ? title.current.value : '',
-    content: content ? content.current.value : '',
+  const [validationState, setValidationState] = useState(initialValidationState);
+
+  const Validation = submittedData => {
+    let isEveryThingValid = false;
+
+    const isNameValid = submittedData.name.trim() !== '';
+    const isPhoneValid = true; // 전화번호 유효성 검사 넣기
+    const isEmailValid = true; // 이메일 유효성 검사 넣기
+    const isTitleValid = submittedData.title.trim() !== '';
+    const isContentValid = submittedData.content.trim() !== '';
+
+    isEveryThingValid =
+      isContentValid && isEmailValid && isTitleValid && isPhoneValid && isNameValid;
+
+    setValidationState(prevState => ({
+      ...prevState,
+      isNameValid,
+      isPhoneValid,
+      isEmailValid,
+      isTitleValid,
+      isContentValid,
+    }));
+
+    return isEveryThingValid;
+  };
+
+  const enterdDataHandler = event => {
+    event.preventDefault();
+    const submittedData = {
+      type: enteredData.type.current.value,
+      name: enteredData.name.current.value,
+      email: enteredData.email.current.value,
+      phoneNumber: enteredData.phoneNumber.current.value,
+      content: enteredData.content.current.value,
+      title: enteredData.title.current.value,
+    };
+
+    const validationResult = Validation(submittedData);
+    // validationResult가 참이면 db에 data저장
+
+    console.log(submittedData);
   };
 
   return (
-    <Form>
-      <TypeLabel>
-        문의 유형
-        <TypeSelect ref={type} autoComplete='off'>
-          <Option value='후원'>후원</Option>
-          <Option value='문의'>문의</Option>
-          <Option value='기타'>기타</Option>
-        </TypeSelect>
-      </TypeLabel>
+    <ContactSection>
+      <Form onSubmit={enterdDataHandler}>
+        <InputLabel>
+          <VerticalCenter>
+            이름 (회사)
+            <Img src='happy.png' alt='웃는 아이콘' />
+            {!validationState.isNameValid ? <Notice /> : null}
+          </VerticalCenter>
+          <Input type='text' name='name' ref={enteredData.name} autoComplete='name' />
+        </InputLabel>
 
-      <InputLabel name='name' ref={name} autoComplete='name'>
-        이름 (회사)
-        <Input />
-      </InputLabel>
+        <InputLabel>
+          <VerticalCenter>
+            연락처
+            <Img src='Phone.png' alt='핸드폰 아이콘' />
+            {!validationState.isPhoneValid ? <Notice /> : null}
+          </VerticalCenter>
+          <Input type='tel' name='phonenumber' ref={enteredData.phoneNumber} autoComplete='tel' />
+        </InputLabel>
 
-      <InputLabel name='phonenumber' ref={phoneNumber} autoComplete='tel'>
-        연락처
-        <Input />
-      </InputLabel>
+        <InputLabel>
+          <VerticalCenter>
+            Email
+            <Img src='Message.png' alt='메세지 아이콘' />
+            {!validationState.isEmailValid ? <Notice /> : null}
+          </VerticalCenter>
+          <Input type='email' name='email' ref={enteredData.email} autoComplete='email' />
+        </InputLabel>
 
-      <InputLabel name='email' ref={email} autoComplete='email'>
-        Email
-        <Input />
-      </InputLabel>
+        <InputLabel>
+          <VerticalCenter>
+            제목
+            <Img src='Check.png' alt='체크모양 아이콘' />
+            {!validationState.isTitleValid ? <Notice /> : null}
+          </VerticalCenter>
+          <Input type='text' name='title' ref={enteredData.title} autoComplete='off' />
+        </InputLabel>
 
-      <InputLabel name='title' ref={title} autoComplete='off'>
-        제목
-        <Input />
-      </InputLabel>
+        <TypeLabel>
+          <TypeSelect ref={enteredData.type} autoComplete='off'>
+            <Option value='선택하지않음'>문의유형</Option>
+            <Option value='후원'>후원</Option>
+            <Option value='문의'>문의</Option>
+            <Option value='기타'>기타</Option>
+          </TypeSelect>
+          {!validationState.name ? <Notice /> : null}
+        </TypeLabel>
 
-      <InputLabel>
-        내용
-        <TextArea name='content' rows={20} ref={content} autoComplete='off' />
-      </InputLabel>
-      <SummitButton type='submit'>SEND</SummitButton>
-    </Form>
+        <InputLabel>
+          <VerticalCenter>
+            내용
+            <Img src='Chat_alt_3.png' alt='대화창 아이콘' width={30} />
+            {!validationState.isContentValid ? <Notice /> : null}
+          </VerticalCenter>
+          <TextArea
+            placeholder='내용을 입력하세요.'
+            name='content'
+            rows={20}
+            ref={enteredData.content}
+            autoComplete='off'
+          />
+        </InputLabel>
+
+        <SubmitButton type='submit'>
+          SEND
+          <ButtonImg src='right_up_arrow.png' alt='제출 화살표' />
+        </SubmitButton>
+      </Form>
+    </ContactSection>
   );
 }
