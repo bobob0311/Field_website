61a204fe69e8f2e96977401196db8961f3f1a738
bobob0311 <qweasdzxc0311@naver.com>
2024-02-16 19:10:44 +0900

    :sparkles: RecruitPage 라우트 설정 및 페이지 생성
    RecruitPage 라우트 설정 및 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,21 @@ import {Route, Routes} from 'react-router-dom';
 import {ThemeProvider} from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 import './index.css';
-import Layout from './layout/Layout';
 import MainPage from './pages/MainPage';
 import theme from './theme';
 import ContactPage from './pages/ContactPage';
 import CampPage from './pages/CampPage';
+import RecruitPage from './pages/RecruitPage';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Routes>
-        <Route element={<Layout />}>
-          <Route path='/' element={<MainPage />} />
-          <Route path='/camp' element={<CampPage />} />
-          <Route path='/contact' element={<ContactPage />} />
-        </Route>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/camp' element={<CampPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/recruit' element={<RecruitPage />} />
       </Routes>
     </ThemeProvider>
   );
