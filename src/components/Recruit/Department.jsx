1124b18a2c3037d58476cf62994c33b710b93354
bobob0311 <qweasdzxc0311@naver.com>
2024-02-20 22:01:42 +0900

    :lipstick: 모집분야 UI 수정 및styled 경고 수정
    모집분야 UI 구정 및 styled 경고 수정

--- src/components/Recruit/Department.jsx
+++ src/components/Recruit/Department.jsx
@@ -7,10 +7,12 @@ const SubTitle = styled.h2`
   text-align: center;
   padding: 0 1rem 0 0;
   margin: 0 0 1rem 0;
-  font-weight: 900;
+  font-weight: 800;
 `;
 
 const QuestionBox = styled.div`
+  display: flex;
+  justify-content: center;
   background: rgba(255, 255, 255, 0.7);
   border-radius: 0.65rem;
   color: ${theme.colors.black};
@@ -26,16 +28,13 @@ const Question = styled.h3`
   margin: 0 0 1.25rem 0;
 `;
 
-const AnswerUl = styled.ul`
-  margin: 0 0 0;
-`;
-
 const Answer = styled.li`
   font-size: 1rem;
-  letter-spacing: -0.07rem;
   font-weight: 700;
   margin: 0 0 1rem 0.25rem;
   word-break: keep-all;
+  text-indent: -1rem;
+  padding: 0 1rem 0 0.8rem;
 `;
 
 const ButtonWrapper = styled.div`
@@ -44,8 +43,8 @@ const ButtonWrapper = styled.div`
   margin: 0 0 1rem 0;
 
   ${props =>
-    props.activelink &&
-    `button[name="${props.activelink}"]{
+    props.$activelink &&
+    `button[name="${props.$activelink}"]{
     background: ${theme.colors.gray};
 
   }
@@ -53,14 +52,15 @@ const ButtonWrapper = styled.div`
 `;
 
 const DepartmentButton = styled.button`
-  padding: 0.3rem 0.5rem;
-  font-size: 0.9rem;
+  font-family: 'SUIT';
+  padding: 0.3rem 0.6rem;
+  font-size: 1rem;
   border-radius: 0.5rem;
   appearance: none;
   border: none;
   background: ${theme.colors.black};
   color: ${theme.colors.white};
-  font-weight: 700;
+  font-weight: 600;
 `;
 
 const allDepartment = {
@@ -87,23 +87,23 @@ const allDepartment = {
       '리더십있고 창의력이 넘치시는 분!',
     ],
     activity: [
-      'FIELD 유튜브 고교 산업공학과 진로지도 강연',
+      '고교 산업공학과 진로지도 강연',
       '산업공학과 출신 기업인 인터뷰',
-      'FIELD CAMP 인적 관리 및 기업 컨텍 기획 및 촬영합니다.',
+      'FIELD CAMP 인적 관리 및 기업 컨택',
     ],
   },
   competition: {
     department: '컴페티션부',
     explain: [
-      '타인과 소통하며 협업 하기를 원하는 분!',
-      '인적, 학술적 교류 활성화를 중요시 하는 분!',
-      '컨텐츠 제작 및 기획에 관심이 많으신 분!',
-      '리더십있고 창의력이 넘치시는 분!',
+      'FIELD 활동에 대한 의지가 강하신 분!',
+      '학술적 정보 제공을 중요시 하는 분!',
+      '세미나를 직접 기획하고 발표한 경험이 있는 분!',
+      '자기 주장력이 강하고 봉사심이 투철하신 분!',
     ],
     activity: [
-      'FIELD 유튜브 고교 산업공학과 진로지도 강연',
-      '산업공학과 출신 기업인 인터뷰',
-      'FIELD CAMP 인적 관리 및 기업 컨텍 기획 및 촬영합니다.',
+      'FIELD 세미나 및 학술 교류 주관',
+      'FIELD CAMP 컴페티션 주제 선정',
+      'FIELD CAMP 컴페티션 자료, 평가 기준 제작',
     ],
   },
   relation: {
@@ -115,9 +115,9 @@ const allDepartment = {
       '리더십있고 창의력이 넘치시는 분!',
     ],
     activity: [
-      'FIELD 유튜브 고교 산업공학과 진로지도 강연',
-      '산업공학과 출신 기업인 인터뷰',
-      'FIELD CAMP 인적 관리 및 기업 컨텍 기획 및 촬영합니다.',
+      '산업공학과 및 FIELD 홍보 카드뉴스 제작',
+      'FIELD 활동 책자 및 신문 제작',
+      'FIELD CAMP 홍보물 제작 및 홍보',
     ],
   },
 };
@@ -125,12 +125,14 @@ const allDepartment = {
 function DepartmentBox({part, p, target}) {
   return (
     <QuestionBox>
-      <Question>{`${allDepartment[part].department}${p}`}</Question>
-      <AnswerUl>
-        {allDepartment[part][target].map((item, index) => (
-          <Answer key={index}>{`${index + 1}. ${item}`}</Answer>
-        ))}
-      </AnswerUl>
+      <div>
+        <Question>{`${allDepartment[part].department}${p}`}</Question>
+        <ul>
+          {allDepartment[part][target].map((item, index) => (
+            <Answer key={index}>{`${index + 1}. ${item}`}</Answer>
+          ))}
+        </ul>
+      </div>
     </QuestionBox>
   );
 }
@@ -144,7 +146,7 @@ export default function Department() {
   return (
     <>
       <SubTitle>모집 분야</SubTitle>
-      <ButtonWrapper activelink={selectedDepartment}>
+      <ButtonWrapper $activelink={selectedDepartment}>
         <DepartmentButton name='planning' onClick={() => DepartmentHandler('planning')}>
           기획부
         </DepartmentButton>
