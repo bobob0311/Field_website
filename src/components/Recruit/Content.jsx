ed25f324910ed83d45437d528bd4e904d221995e
bobob0311 <qweasdzxc0311@naver.com>
2024-02-20 20:18:54 +0900

    :lipstick: 마진수정에 따른 스타일 수정 및 마크업 수정
    마진수정에 따른 스타일 수정 및 마크업 수정

--- src/components/Recruit/Content.jsx
+++ src/components/Recruit/Content.jsx
@@ -3,23 +3,6 @@ import {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import theme from '../../theme';
 
-const Box = styled.div`
-  background: rgba(255, 255, 255, 0.7);
-  border-radius: 0.65rem;
-  padding: 0.5rem;
-`;
-
-const Wrapper = styled.div`
-  margin: 5rem 0;
-`;
-const SubTitle = styled.h2`
-  font-size: 1.5rem;
-  text-align: center;
-  padding: 0 1rem 0 0;
-  margin: 0 0 1rem 0;
-  font-weight: 900;
-`;
-
 const P = styled.p`
   font-size: ${props => (props.fontSize ? props.fontSize : '1rem')};
   color: ${props => (props.color ? theme.colors[props.color] : theme.colors.black)};
@@ -29,7 +12,25 @@ const P = styled.p`
   word-break: keep-all;
 `;
 
-const Pspan = styled.span`
+const ContentBox = styled.div`
+  background: rgba(255, 255, 255, 0.7);
+  border-radius: 0.65rem;
+  padding: 0.5rem;
+`;
+
+const ContentWrapper = styled.section`
+  margin: 5rem 0;
+`;
+const SubTitle = styled.h2`
+  font-size: 1.5rem;
+  text-align: center;
+  padding: 0 1rem 0 0;
+  margin: 0 0 1.5rem 0;
+  font-weight: 800;
+`;
+
+const OneLine = styled.span`
+  display: block;
   margin: 0 0 0.2rem 0;
   text-indent: ${props => (props.textIndent ? props.textIndent : '')};
 `;
@@ -38,6 +39,7 @@ const FlexRow = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin: 0 0 0.5rem 0;
 `;
 
 const WhiteMessage = styled.img`
@@ -56,26 +58,44 @@ const AddressLink = styled.a`
   color: ${theme.colors.white};
   font-size: 1rem;
   font-weight: 700;
+  margin: 0 0.4rem 0 0;
 `;
 
 function Contanier({subtitle, content}) {
   return (
-    <Wrapper>
+    <ContentWrapper>
       <SubTitle>{subtitle}</SubTitle>
       <P color='white'>{content}</P>
-    </Wrapper>
+    </ContentWrapper>
   );
 }
-function ContanierWithBox({subtitle, content}) {
+function ContanierWithContentBox({subtitle, content}) {
   return (
-    <Wrapper>
+    <ContentWrapper>
       <SubTitle>{subtitle}</SubTitle>
-      <Box>
-        <P color='white'>{content}</P>
-      </Box>
-    </Wrapper>
+      <ContentBox>{content}</ContentBox>
+    </ContentWrapper>
   );
 }
+
+const APPLYMETHOD = (
+  <>
+    <P fontWeight='700' margin='0.5rem 0 0.5rem 0'>
+      <OneLine>필드 리틀리 혹은 필드 블로그에서 지원서 </OneLine>
+      <OneLine>다운로드 후 서류 작성하여 아래 이메일로 제출</OneLine>
+    </P>
+    <FlexRow>
+      👉
+      <WhiteMessage src='MessageWhite.png' alt='하얀색 Messege 아이콘' width={20} />
+      <address>
+        <AddressLink href='mailto:iefieldcamp24@gmail.com' target='_blank'>
+          iefieldcamp24@gmail.com
+        </AddressLink>
+      </address>
+      👈
+    </FlexRow>
+  </>
+);
 
 export default function Content() {
   const [data, setData] = useState([]);
@@ -101,41 +121,24 @@ export default function Content() {
       Error(err);
     }
   }
+
   useEffect(() => {
     RecuitData();
   }, []);
 
-  const APPLYMETHOD = (
-    <>
-      <P fontWeight='700' margin='0.5rem 0 0.5rem 0'>
-        <Pspan>필드 리틀리 혹은 필드 블로그에서 지원서</Pspan>
-        <Pspan>다운로드 후 서류 작성하여 아래 이메일로 제출</Pspan>
-      </P>
-      <FlexRow>
-        👉
-        <WhiteMessage src='MessageWhite.png' alt='하얀색 Messege 아이콘' width={20} />
-        <address>
-          <AddressLink href='mailto:iefieldcamp24@gamil.com' target='_blank'>
-            iefieldcamp24@gamil.com
-          </AddressLink>
-        </address>
-        👈
-      </FlexRow>
-    </>
-  );
-  const DATE = (
+  const recruitDate = (
     <>
       <DateP>
         📄서류 접수: {data[0]} ~ {data[1]}
       </DateP>
       <DateP>✅1차 서류 전형 합격자 발표: {data[2]}</DateP>
       <DateP>
-        <Pspan>
+        <OneLine>
           💬2차 면접: {data[3]} ~ {data[4] ? data[4].slice(8) : ''}
-        </Pspan>
-        <Pspan textIndent='4.8rem'>
+        </OneLine>
+        <OneLine textIndent='4.8rem'>
           {data[5]} ~ {data[6] ? data[6].slice(8) : ''}
-        </Pspan>
+        </OneLine>
       </DateP>
     </>
   );
@@ -149,9 +152,9 @@ export default function Content() {
         필드는 매년 1월에 새로운 멤버를 모집합니다.
       </P>
       <Contanier subtitle='😀 지원자격' content='산업공학을 주/복수/부전공하는 대학생' />
-      <ContanierWithBox subtitle='📆 모집 일정' content={DATE} />
+      <ContanierWithContentBox subtitle='📆 모집 일정' content={recruitDate} />
       <Contanier subtitle='📚 활동 기간' content='매년 3월 ~ 12월 (10개월)' />
-      <ContanierWithBox subtitle='💎 지원 방법' content={APPLYMETHOD} />
+      <ContanierWithContentBox subtitle='💎 지원 방법' content={APPLYMETHOD} />
     </>
   );
 }
