4e5f5d66bf602749158d45c65617fdb1b979c6ab
kkk1k <googija9818@gmail.com>
2024-03-09 04:22:28 +0900

    :sparkles: 로컬 스토리지 시간 초기화 로직  추가
    로컬 스토리지 시간 초기화 추가

--- src/pages/NewsPage.jsx
+++ src/pages/NewsPage.jsx
@@ -160,7 +160,6 @@ export default function NewsPage() {
     const urlCategory = new URLSearchParams(location.search).get('category') || '월간필드';
     getDataNews(urlCategory);
     setSelectCategory(urlCategory);
-    console.log(newsData);
     setLoading(false);
   }, [location.search]);
 
