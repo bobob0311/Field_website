1a0d7c6c910039f0075a51072f2c1e10767c531b
Hankyeong Kim <122015488+kkk1k@users.noreply.github.com>
2024-03-11 17:30:58 +0900

    Revert "Pr revert"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import ReactDOM from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
-import App from "./App";
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import App from './App';
+import {store} from './redux/store/store';
+import ScrollToTop from './components/ScrollToTop';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
+    </Provider>
   </BrowserRouter>,
 );
